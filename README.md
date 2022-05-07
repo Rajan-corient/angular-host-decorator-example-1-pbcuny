e23b54d665487a4fe0e12793287869c93f73f56d
Rajan-corient <rajan.gupta@corientbs.com>
2022-05-07 19:46:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-host-decorator-example-1-pbcuny
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-host-decorator-example-1-pbcuny)
---
+++ src/app/grand-child.component.ts
@@ -0,0 +1,22 @@
+import { Component, SkipSelf, Self, Optional, Host } from "@angular/core";
+import { RandomService } from "./random-service";
+
+@Component({
+  selector: "my-grandChild",
+  template: `
+    <div class="box">
+      GrandChildComponent => {{ randomNo }}
+
+      <div class="dirbox" testDirective>fdf</div>
+
+    </div>
+  `,
+  providers: [],
+  viewProviders: [],
+})
+export class GrandChildComponent {
+  randomNo;
+  constructor(private randomService: RandomService) {
+    this.randomNo = randomService?.RandomNo;
+  }
+}
